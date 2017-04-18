ab6d771f7fa72ccd7aa723b1c03bb76e7a01ecaa
sahadev <sahadev.nawar@capgemini.com>
2017-04-18 09:23:08 +0200

    First commit: server
    Jira automation spring boot server

---
+++ src/main/java/com/example/JiraApplication.java
@@ -0,0 +1,22 @@
+package com.example;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.support.SpringBootServletInitializer;
+import org.springframework.context.annotation.ComponentScan;
+
+
+@SpringBootApplication
+public class JiraApplication extends SpringBootServletInitializer {
+
+    @Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+        return application.sources(JiraApplication.class);
+    }
+
+    public static void main(String[] args) throws Exception {
+        SpringApplication.run(JiraApplication.class, args);
+    }
+
+}
---
+++ src/main/java/com/example/web/RestController.java
@@ -0,0 +1,13 @@
+package com.example.web;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@org.springframework.web.bind.annotation.RestController
+public class RestController {
+	
+	
+ @RequestMapping("/api/hello")
+ public String greet() {
+  return "Hiiiiiiiiiiiiiiii  gyugyuyugy!!!";
+ }
+}
